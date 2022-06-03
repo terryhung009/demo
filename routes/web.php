0d0585e1e29054415f34bf91d50b85b3b200dccf
dannyHung <khc741213@gmail.com>
2022-06-03 14:49:53 +0800

    update ep6

--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,39 @@ use Inertia\Inertia;
 |
 */
 
+// Route::get('/', function () {
+//     return inertia::render('Home', 
+// [
+//         'name' => 'Jeffrey Way',
+//         'frameworks' => [
+//             'Laravel', 'Vue', 'Inertia'
+//         ]
+//     ]);
+//     // return View::make('Welcome');
+//     // return inertia('Welcome');
+// });
 Route::get('/', function () {
-    return inertia::render('Home', [
-        'name' => 'Jeffrey Way',
-        'frameworks' => [
-            'Laravel', 'Vue', 'Inertia'
-        ]
-    ]);
-    // return View::make('Welcome');
-    // return inertia('Welcome');
+    return inertia::render(
+        'Home'
+    );
+});
+
+Route::get('/users', function () {
+    sleep(2);
+
+    return inertia::render(
+        'Users'
+    );
+});
+
+Route::get('/settings', function () {
+    sleep(2);
+
+    return inertia::render(
+        'Settings'
+    );
+});
+Route::post('/logout', function () {
+
+    dd('logging the user out');
 });
